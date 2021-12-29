b205f75054269cf45e1fb4094d4007f4e5c898e5
cloud <50234737+rhqtmfajfl@users.noreply.github.com>
2021-12-29 12:31:57 +0900

    Create day 3 ResponseServlet.java

---
+++ Day 2 Servlet_jsp/day 3 ResponseServlet.java
@@ -0,0 +1,61 @@
+package base;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.ServletOutputStream;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet({ "/getHTML", "/getXML", "/getJSON", "/getImage" })   //servlet은 하난데 url case는 4가지로 나눠진다.
+
+
+public class ResponseServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		String uri = request.getRequestURI();  //클라이언트가 사용하는 uri 부분을 꺼내고 있다.
+		System.out.println(uri);
+		String filename = "";
+		String contentType = "";
+		if (uri.endsWith("getHTML")) {   //추출된 uri가 getHTML로 끝나면 밑에 부분으로 출력해라.
+			filename = "c:/Temp/sample.html";	
+			contentType = "text/html; charset=utf-8";
+		} else if (uri.endsWith("getXML")) { //추출된 uri가 getXML로 끝나면 밑에 부분으로 출력해라.
+			filename = "c:/Temp/sample.xml";	
+			contentType = "application/xml; charset=utf-8";
+		} else if (uri.endsWith("getJSON")) { //추출된 uri가 getJSON로 끝나면 밑에 부분으로 출력해라.
+			filename = "c:/Temp/sample.json";	
+			contentType = "text/json; charset=utf-8";
+		} else {
+			filename = "c:/Temp/trans_duke.png";	   //file IO 운영체제에 알맞는 PATH 정보
+			contentType = "image/png";
+		}
+		File f = new File(filename);
+		FileInputStream fis = new FileInputStream(f);
+		response.setContentType(contentType);
+		if(contentType.startsWith("image")) {
+			byte[] content = new byte[(int)f.length()];
+			ServletOutputStream sos = response.getOutputStream();
+			fis.read(content);
+			sos.write(content);			
+			sos.close();
+		} else {
+			InputStreamReader isr = new InputStreamReader(fis, "utf-8");
+			BufferedReader br = new BufferedReader(isr);
+			PrintWriter out = response.getWriter();
+			String line = null; 
+			while((line = br.readLine()) != null) 
+				out.println(line);
+			out.close();
+			br.close();
+			isr.close();			
+		}		
+		fis.close();
+	}
+}
