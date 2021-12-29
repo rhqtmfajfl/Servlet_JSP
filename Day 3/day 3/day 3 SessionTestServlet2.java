505c06c2934bede798fe47052868030de1b31b23
cloud <50234737+rhqtmfajfl@users.noreply.github.com>
2021-12-29 15:26:13 +0900

    Update day 3 SessionTestServlet2.java

--- Day 3/day 3/day 3 SessionTestServlet2.java
+++ Day 3/day 3/day 3 SessionTestServlet2.java
@@ -18,18 +18,28 @@ public class SessionTestServlet2 extends HttpServlet {
 		HttpSession session = request.getSession();
 		response.setContentType("text/html; charset=utf-8");
 		PrintWriter out = response.getWriter();	
-		out.print("생성된 세션객체의 ID : "+session.getId());
-		out.print("<br>세션 객체가 생성된 시간 : "
-				      + new Date(session.getCreationTime()));
+		out.print("생성된 세션객체의 ID : "+session.getId());  //세션 아이디에 대한 코드 getId()
+		out.print("<br>세션 객체가 생성된 시간 : "   //
+				      + new Date(session.getCreationTime()));  //이 모트를 통해서 세션객체가 만들어진 시간
 		out.print("<br>현재 시간 : " + new Date());
 		out.close();
 	}
 }
 
 
+//////////////////////////////////
+
+세션객체 id 와
 
 
+세션객체가 만들어진 시간은 변함ㅇ ㅣ없다.
+	
+	
+그런데 이때 크롬에서의 세션 id 와 edge에서의 세션 id의 
 
+세션객체가 생성된 시간이 다르다 그리고 id도 다르다.
+	
+	
 
 
 
